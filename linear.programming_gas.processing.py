7f2f1ebe8d93cbc4977db5b61098decf887186a3
R.A.Borrelli <r.angelo.borrelli@gmail.com>
2015-09-10 10:18:07 -0600

    Max profit

--- linear.programming_gas.processing.py
+++ linear.programming_gas.processing.py
@@ -36,6 +36,9 @@ premium_gas_productionplot=numpy.zeros(100)
 #
 regular_gas_storageplot=numpy.zeros((100,2))
 premium_gas_storageplot=numpy.zeros((100,2))
+#
+regular_gas_profitplot=numpy.zeros(100)
+premium_gas_profitplot=numpy.zeros(100)
 #######
 #
 #
@@ -58,6 +61,8 @@ premium_gas_storage=6 #ton
 ####### objective function
 regular_gas_profit=150 #$/ton
 premium_gas_profit=175 #$/ton
+#
+maximum_profit=1418
 #######
 #
 #
@@ -73,11 +78,13 @@ for i in range(0,100):
     regular_gas_productionplot[i]=j
     regular_gas_storageplot[i,1]=j
     premium_gas_storageplot[i,0]=j
+    regular_gas_profitplot[i]=j
 #
     premium_gas_volplot[i]=(float(volume_constraint)/float(premium_gas_volume))-(float(regular_gas_volume)/float(premium_gas_volume))*regular_gas_volplot[i]
     premium_gas_productionplot[i]=(float(production_time)/float(premium_gas_production_time))-(float(regular_gas_production_time)/float(premium_gas_production_time))*regular_gas_productionplot[i]
     regular_gas_storageplot[i,0]=regular_gas_storage
     premium_gas_storageplot[i,1]=premium_gas_storage
+    premium_gas_profitplot[i]=(float(maximum_profit)/float(premium_gas_profit))-(float(regular_gas_profit)/float(premium_gas_profit))*regular_gas_profitplot[i]
 #
     j=j+1
 # end
@@ -97,6 +104,7 @@ line_color1='Blue'
 line_color2='Red'
 line_color3='Black'
 line_color4='Green'
+line_color5='Purple'
 ###
 plot.title(title,fontsize=18)
 left_axis.set_xlabel(xtitle,fontsize=16)
@@ -105,10 +113,10 @@ left_axis.set_ylabel(ytitle,fontsize=16)
 ###
 # axis
 xmin=0
-xmax=15
+xmax=11
 #
 ymin=0
-ymax=15
+ymax=11
 #
 xmajortick=1
 ymajortick=1
@@ -135,6 +143,7 @@ left_axis.plot(regular_gas_volplot,premium_gas_volplot,color=line_color1,label='
 left_axis.plot(regular_gas_productionplot,premium_gas_productionplot,color=line_color2,label='Production constraint: 10r+8p=80')
 left_axis.plot(regular_gas_storageplot[:,0],regular_gas_storageplot[:,1],color=line_color3,label='Regular gas storage: r=9')
 left_axis.plot(premium_gas_storageplot[:,0],premium_gas_storageplot[:,1],color=line_color4,label='Premium gas storage: p=6')
+left_axis.plot(regular_gas_profitplot,premium_gas_profitplot,color=line_color5,label='Maximum profit: 150r+175p=1418')
 ###
 pylab.legend(loc='upper right')
 plot.get_current_fig_manager().resize(1920,1080)
